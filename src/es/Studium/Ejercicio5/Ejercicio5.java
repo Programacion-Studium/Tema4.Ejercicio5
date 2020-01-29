d35aefaff0b83315ae06ce1ea8eb74e7784ecf21
Alvca <Alvca@DESKTOP-H3LD864>
2020-01-29 10:01:31 +0100

    1

--- src/es/Studium/Ejercicio5/Ejercicio5.java
+++ src/es/Studium/Ejercicio5/Ejercicio5.java
@@ -1,13 +1,18 @@
 package es.Studium.Ejercicio5;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 /**
  * @author alvaro
  *
  */
-public class Ejercicio5 extends Frame
+public class Ejercicio5 extends Frame implements WindowListener, ItemListener
 {
 	private static final long serialVersionUID = 1L;
-	// CreaciÃ³n de una lista con 4 elementos visibles y que no
+	// Creación de una lista con 4 elementos visibles y que no
 	// permite multiselecciÃ³n
 	Choice choLista = new Choice(); //Esto indica la cantidad de elementos que seran visibles, el false indica si podemos o no marcar mas de un elemento de la lista.
 	public Ejercicio5()
@@ -15,17 +20,76 @@ public class Ejercicio5 extends Frame
 		setLayout(new FlowLayout());
 		setTitle("Lista"); //Titulo de la ventana
 		//AÃ±adimos los elementos de la lista
-		choLista.add("Ãlava");
+		choLista.add("Elegir una");
+		choLista.add("Álava");
 		choLista.add("Albacete");
 		choLista.add("Alicante");
 		choLista.add("Zaragoza");
-		add(choLista); //AÃ±adimos la lista
+		add(choLista); //Añadimos la lista
 		setLocationRelativeTo(null);
 		setSize(150,150);
+
+		choLista.addItemListener(this);
+		this.addWindowListener(this);
+		
+
 		setVisible(true);
 	}
 	public static void main(String[] args)
 	{
 		new Ejercicio5(); //Mostramos la lista
+	}
+	@Override
+	public void itemStateChanged(ItemEvent ie) {
+		// TODO Auto-generated method stub
+		String gentilicio= new String();
+		if ("Álava".equals(ie.getItem())) {
+			gentilicio = "Alavés y Alavesa";
+		}
+		if ("Albacete".equals(ie.getItem())) {
+			gentilicio = "Albaceteño y Albaceteña";
+		}
+		if ("Alicante".equals(ie.getItem())) {
+			gentilicio = "Alicantino y Alicantina";
+		}
+		if ("Zaragoza".equals(ie.getItem())) {
+			gentilicio = "Zaragozano y Zaragozana";
+		}
+		System.out.println(gentilicio);	
+	}
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowClosing(WindowEvent e) {
+		// TODO Auto-generated method stub
+		System.exit(0); 
+	}
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
